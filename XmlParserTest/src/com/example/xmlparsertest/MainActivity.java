9cb1a9996ccd08237ff12efda5c03a92ec9d7914
Byungmin.Kim <Byungmin.Kim@192.168.1.101>
2014-11-16 00:08:30 +0900

    Test

--- XmlParserTest/src/com/example/xmlparsertest/MainActivity.java
+++ XmlParserTest/src/com/example/xmlparsertest/MainActivity.java
@@ -5,29 +5,24 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.net.ssl.HttpsURLConnection;
-
-import org.apache.http.HttpConnection;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 	private final String TAG = "Main";
-	private String isbnUrl = "http://book.interpark.com/api/search.api";
-	private Map<String, String> params = new HashMap<String, String>();
-	
+	private final String mIsbnUrl = "http://book.interpark.com/api/search.api";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,12 +30,18 @@ public class MainActivity extends Activity {
 		
 		ArrayList<BookItem> items = new ArrayList<BookItem>();
 		try {
-			params.put("key", "672DC494E29F75F40A38931508CD19AF96653A82A502146C58A62766FFA2AC61");
-			params.put("query", "9788996603139");
+//			String isbnCode = "9788996603139";
+			String isbnCode = "9788932916200";
+
+			String userKey = "672DC494E29F75F40A38931508CD19AF96653A82A502146C58A62766FFA2AC61";
+			
+			Map<String, String> params = new HashMap<String, String>();
+			
+			params.put("key", userKey);
+			params.put("query", isbnCode);
 			params.put("queryType", "isbn");
 			
-//			new PullParserTask("http://book.interpark.com/api/search.api?key=672DC494E29F75F40A38931508CD19AF96653A82A502146C58A62766FFA2AC61&query=9788996603139&queryType=isbn", items).execute();
-			new PullParserTask(isbnUrl, params, items).execute();
+			new PullParserTask(mIsbnUrl, params, items).execute();
 
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
@@ -55,12 +56,10 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	
 	public class PullParserTask extends AsyncTask<Void, Void, Void>{
 
 		private ArrayList<BookItem> mBookItems = null;
 		private URL mURL = null;
-		private Map<String,String> mParams = null;
 		
 		public PullParserTask(String url, Map<String,String> params, ArrayList<BookItem> bookItems) throws MalformedURLException {
 			this.mURL = new URL(getGetURLString(url, params));
@@ -79,26 +78,10 @@ public class MainActivity extends Activity {
 					sb.append(params.get(key));
 					sb.append("&");
 				}
+			}
 			
-				sb.deleteCharAt(sb.length() - 1);
-			}
 			Log.i(TAG,"[getGetURLString]url : " + sb.toString());
-
 			return sb.toString();
-		}
-		
-		@Override
-		protected void onPostExecute(Void result) {
-			for(BookItem item : mBookItems){
-				Log.i(TAG,"[onPostExecute]title : " + item.title);
-				Log.i(TAG,"[onPostExecute]description : " + item.description);
-				Log.i(TAG,"[onPostExecute]pubDate : " + item.pubDate);
-				Log.i(TAG,"[onPostExecute]coverSmallUrl : " + item.coverSmallUrl);
-				Log.i(TAG,"[onPostExecute]coverLargeUrl : " + item.coverLargeUrl);
-				Log.i(TAG,"[onPostExecute]publisher : " + item.publisher);
-				Log.i(TAG,"[onPostExecute]author : " + item.author);
-				Log.i(TAG,"[onPostExecute]isbn : " + item.isbn);
-			}
 		}
 
 		@Override
@@ -113,16 +96,16 @@ public class MainActivity extends Activity {
 				InputStream in = urlConnection.getInputStream();
 				parser.setInput(in, "utf-8");
 				
-				String tagName = "";
 				int eventType = parser.getEventType();
 				boolean isItemTag = false;
-				BookItem item = null;
 				
+				BookItem item = null;
+				String tagName = "";
+
 				while(eventType != XmlPullParser.END_DOCUMENT){
 					
 					if(eventType == XmlPullParser.START_TAG){
 						tagName = parser.getName();
-						Log.i(TAG,"XmlPullParser.START_TAG : " + tagName);
 
 						if(tagName.equals("item")){
 							item = new BookItem();
@@ -131,7 +114,6 @@ public class MainActivity extends Activity {
 						
 					}else if(eventType == XmlPullParser.END_TAG){
 						tagName = parser.getName();
-						Log.i(TAG,"XmlPullParser.END_TAG : " + tagName);
 
 						if(tagName.equals("item")){
 							mBookItems.add(item);
@@ -144,39 +126,30 @@ public class MainActivity extends Activity {
 						}
 						
 					}else if(eventType == XmlPullParser.TEXT && isItemTag){
-						Log.i(TAG,"XmlPullParser.TEXT : " + tagName);
 						
 						if(tagName.equals("title")){
 							item.title = parser.getText();
-							Log.i(TAG,"[onPostExecute]XmlPullParser.TEXT : " + item.title);
 
 						}else if(tagName.equals("description")){
 							item.description = parser.getText();
-							Log.i(TAG,"[onPostExecute]XmlPullParser.TEXT : " + item.description);
 
 						}else if(tagName.equals("pubDate")){
 							item.pubDate = parser.getText();
-							Log.i(TAG,"[onPostExecute]XmlPullParser.TEXT : " + item.pubDate);
 
 						}else if(tagName.equals("coverSmallUrl")){
 							item.coverSmallUrl = parser.getText();
-							Log.i(TAG,"[onPostExecute]XmlPullParser.TEXT : " + item.coverSmallUrl);
 
 						}else if(tagName.equals("coverLargeUrl")){
 							item.coverLargeUrl = parser.getText();
-							Log.i(TAG,"[onPostExecute]XmlPullParser.TEXT : " + item.coverLargeUrl);
 
 						}else if(tagName.equals("publisher")){
 							item.publisher = parser.getText();
-							Log.i(TAG,"[onPostExecute]XmlPullParser.TEXT : " + item.publisher);
 
 						}else if(tagName.equals("author")){
 							item.author = parser.getText();
-							Log.i(TAG,"[onPostExecute]XmlPullParser.TEXT : " + item.author);
 
 						}else if(tagName.equals("isbn")){
 							item.isbn = parser.getText();
-							Log.i(TAG,"[onPostExecute]XmlPullParser.TEXT : " + item.isbn);
 
 						}
 					}
@@ -194,6 +167,19 @@ public class MainActivity extends Activity {
 			return null;
 		}
 		
+		@Override
+		protected void onPostExecute(Void result) {
+			for(BookItem item : mBookItems){
+				Log.i(TAG,"[onPostExecute]title : " + item.title);
+				Log.i(TAG,"[onPostExecute]description : " + item.description);
+				Log.i(TAG,"[onPostExecute]pubDate : " + item.pubDate);
+				Log.i(TAG,"[onPostExecute]coverSmallUrl : " + item.coverSmallUrl);
+				Log.i(TAG,"[onPostExecute]coverLargeUrl : " + item.coverLargeUrl);
+				Log.i(TAG,"[onPostExecute]publisher : " + item.publisher);
+				Log.i(TAG,"[onPostExecute]author : " + item.author);
+				Log.i(TAG,"[onPostExecute]isbn : " + item.isbn);
+			}
+		}
 	}
 	
 	public class BookItem{
